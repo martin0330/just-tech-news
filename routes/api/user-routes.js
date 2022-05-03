180305cebfa6798d7cdd2a26e364a8c4574a818e
martin0330 <martin.cruz0330@yahoo.com>
2022-05-02 21:57:15 -0500

    added post routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,9 +1,8 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post } = require('../../models');
 
-// GET /api/users
+// get all users
 router.get('/', (req, res) => {
-    // Access our User model and run .findAll() method)
     User.findAll({
         attributes: { exclude: ['password'] }
     })
@@ -14,13 +13,18 @@ router.get('/', (req, res) => {
         });
 });
 
-// GET /api/users/1
 router.get('/:id', (req, res) => {
     User.findOne({
         attributes: { exclude: ['password'] },
         where: {
             id: req.params.id
-        }
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_url', 'created_at']
+            }
+        ]
     })
         .then(dbUserData => {
             if (!dbUserData) {
@@ -35,7 +39,6 @@ router.get('/:id', (req, res) => {
         });
 });
 
-// POST /api/users
 router.post('/', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
     User.create({
@@ -63,6 +66,7 @@ router.post('/login', (req, res) => {
         }
 
         const validPassword = dbUserData.checkPassword(req.body.password);
+
         if (!validPassword) {
             res.status(400).json({ message: 'Incorrect password!' });
             return;
@@ -72,8 +76,6 @@ router.post('/login', (req, res) => {
     });
 });
 
-
-// PUT /api/users/1
 router.put('/:id', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
@@ -84,9 +86,19 @@ router.put('/:id', (req, res) => {
             id: req.params.id
         }
     })
-})
+        .then(dbUserData => {
+            if (!dbUserData[0]) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
-// DELETE /api/users/1
 router.delete('/:id', (req, res) => {
     User.destroy({
         where: {
